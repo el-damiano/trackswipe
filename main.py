1db301eafc17d7cc30a587ebff6d6c8f698b6b97
el-damiano <el9damiano@gmail.com>
2025-01-19 21:54:03 +0100

    feat: simulate key presses

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import math
 import pygame
 from pygame.math import Vector2
+from evdev import uinput, ecodes as e
 
 
 def map_vector_to_key(mappings: list, target: pygame.Vector2, source = Vector2(0.0, 0.0)):
@@ -13,8 +14,9 @@ def map_vector_to_key(mappings: list, target: pygame.Vector2, source = Vector2(0
     nan_to_zero(source)
     nan_to_zero(target)
 
+    # TODO: figure out how to properly divide it
     def part(x):
-        return (x > 0.43) + (x >= -0.33)
+        return (x > 0.33) + (x >= -0.33)
 
     target_x = part(target.x)
     target_y = part(target.y)
@@ -31,53 +33,53 @@ def main():
     KEYMAP = [
         [
             [
-                ["s", "", ""],
-                ["", "w", ""],
-                ["", "", ""],
+                [e.KEY_S, None, None],
+                [None, e.KEY_W, None],
+                [None, None, None],
             ],
             [
-                ["", "r", ""],
-                ["", "g", ""],
-                ["", "", ""],
+                [None, e.KEY_R, None],
+                [None, e.KEY_G, None],
+                [None, None, None],
             ],
             [
-                ["", "", "o"],
-                ["", "u", ""],
-                [" ", "", ""],
-            ],
-        ],
-        [
-            [
-                ["", "", ""],
-                ["n", "m", ""],
-                ["", "", ""],
-            ],
-            [
-                ["j", "q", "b"],
-                ["k", "h", "p"],
-                ["v", "x", "y"],
-            ],
-            [
-                ["", "", " "],
-                ["", "l", "a"],
-                ["", " ", "\n"],
+                [None, None, e.KEY_O],
+                [None, e.KEY_U, None],
+                [None, None, None],
             ],
         ],
         [
             [
-                ["", "", " "],
-                ["", "c", ""],
-                ["t", "", ""],
+                [None, None, None],
+                [e.KEY_N, e.KEY_M, None],
+                [None, None, None],
             ],
             [
-                ["", "", ""],
-                ["", "f", ""],
-                [",", "i", "z"],
+                [e.KEY_J, e.KEY_Q, e.KEY_B],
+                [e.KEY_K, e.KEY_H, e.KEY_P],
+                [e.KEY_V, e.KEY_X, e.KEY_Y],
             ],
             [
-                ["", "", ""],
-                ["", "d", ""],
-                ["", "", "e"],
+                [None, e.KEY_ENTER, None],
+                [None, e.KEY_L, e.KEY_A],
+                [e.KEY_BACKSPACE, e.KEY_BACKSPACE, e.KEY_SPACE],
+            ],
+        ],
+        [
+            [
+                [None, None, None],
+                [None, e.KEY_C, None],
+                [e.KEY_T, None, None],
+            ],
+            [
+                [None, None, None],
+                [None, e.KEY_F, None],
+                [None, e.KEY_I, e.KEY_Z],
+            ],
+            [
+                [None, None, None],
+                [None, e.KEY_D, None],
+                [None, None, e.KEY_E],
             ],
         ]
     ]
@@ -89,54 +91,62 @@ def main():
     first_selection = None
     final_selection = None
 
-    while listening:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                listening = False
+    with uinput.UInput() as ui:
+        while listening:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    listening = False
 
-            if event.type == pygame.JOYDEVICEADDED:
-                joy = pygame.joystick.Joystick(event.device_index)
-                joysticks[joy.get_instance_id()] = joy
+                if event.type == pygame.JOYDEVICEADDED:
+                    joy = pygame.joystick.Joystick(event.device_index)
+                    joysticks[joy.get_instance_id()] = joy
 
-            if event.type == pygame.JOYDEVICEREMOVED:
-                del joysticks[event.instance_id]
+                if event.type == pygame.JOYDEVICEREMOVED:
+                    del joysticks[event.instance_id]
 
-        for joystick in joysticks.values():
-            B = joystick.get_button(3)
-            if B:
-                listening = False
+            for joystick in joysticks.values():
+                B = joystick.get_button(3)
+                if B:
+                    listening = False
 
-            # TRACKPAD_LEFT = {
-            #     "pressed": joystick.get_button(0),
-            #     "x": joystick.get_axis(4),
-            #     "y": joystick.get_axis(5),
-            # }
+                # TRACKPAD_LEFT = {
+                #     "pressed": joystick.get_button(0),
+                #     "x": joystick.get_axis(4),
+                #     "y": joystick.get_axis(5),
+                # }
 
-            TRACKPAD_RIGHT = {
-                "pressed": joystick.get_button(1),
-                "x": joystick.get_axis(2),
-                "y": joystick.get_axis(3),
-            }
+                TRACKPAD_RIGHT = {
+                    "pressed": joystick.get_button(1),
+                    "x": joystick.get_axis(2),
+                    "y": joystick.get_axis(3),
+                }
 
-            trackpad_pressed = TRACKPAD_RIGHT["pressed"]
-            key_was_selected = (
-                not trackpad_pressed and
-                first_selection is not None and
-                final_selection is not None
-            )
+                trackpad_pressed = TRACKPAD_RIGHT["pressed"]
+                key_was_selected = (
+                    not trackpad_pressed and
+                    first_selection is not None and
+                    final_selection is not None
+                )
 
-            if trackpad_pressed:
-                target = pygame.Vector2(TRACKPAD_RIGHT["x"], TRACKPAD_RIGHT["y"])
-                final_selection = target
+                if trackpad_pressed:
+                    target = pygame.Vector2(TRACKPAD_RIGHT["x"], TRACKPAD_RIGHT["y"])
+                    final_selection = target
 
-                if first_selection is None:
-                    first_selection = target
+                    if first_selection is None:
+                        first_selection = target
 
-            elif key_was_selected:
-                print(map_vector_to_key(KEYMAP, final_selection, first_selection))
-                first_selection = None
+                elif key_was_selected:
+                    key = map_vector_to_key(KEYMAP, final_selection, first_selection)
+                    first_selection = None
+                    if key is None:
+                        continue
 
-        clock.tick(15)
+                    ui.write(e.EV_KEY, key, 1)
+                    ui.syn()
+                    ui.write(e.EV_KEY, key, 0)
+                    ui.syn()
+
+            clock.tick(15)
 
 
 if __name__ == "__main__":
